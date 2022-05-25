758e29873bad6820c9a80a6f492eb173d85885f7
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-05-25 12:41:08 -0400

    feat: basic homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
 export function App() {
   return (
-    <div class='text-white'>
-      <nav class='sticky top-0 bg-black shadow-md font-mono'>
-        <div class='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-          <div class='relative flex items-center justify-between h-16'>
-            <div class='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
-              <div class='flex-shrink-0 flex items-center'>
-                <div class='text-indigo-500 px-3 py-2 text-2xl'>RealShadowNova</div>
-              </div>
-            </div>
+    <div class='text-white bg-black w-full h-full'>
+      <nav class='sticky top-0 pt-4'>
+        <div class='absolute right-0 mr-8 sm:mr-12 lg:mr-16'>
+          <div class='flex space-x-4'>
+            <a
+              href='https://discord.gg/fERY6AenEv'
+              target='_blank'
+              class='hover:bg-indigo-600 transition ease-in-out rounded-lg block w-auto px-3 py-2'
+            >
+              <img src='../assets/discord.png' height='32' width='32' alt='Discord' />
+            </a>
 
-            <div class='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
-              <div class='ml-3 relative'>
-                <div class='hidden sm:block sm:ml-6'>
-                  <div class='flex space-x-4'>
-                    <a href='https://discord.gg/fERY6AenEv' target='_blank' class='hover:text-indigo-500 block w-auto px-3 py-2 text-base'>
-                      Discord
-                    </a>
-
-                    <a href='https://github.com/RealShadowNova' target='_blank' class='hover:text-indigo-500 block w-auto px-3 py-2 text-base'>
-                      GitHub
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <a
+              href='https://github.com/RealShadowNova'
+              target='_blank'
+              class='hover:shadow-xl hover:bg-indigo-600 transition ease-in-out rounded-lg block w-auto px-3 py-2'
+            >
+              <img src='../assets/github.png' alt='GitHub' />
+            </a>
           </div>
         </div>
       </nav>
+
+      <div class='container min-h-screen p-12 sm:p-24 lg:p-32'>
+        <div class='space-y-8'>
+          <div class='space-y-4'>
+            <div class='text-xl font-mono text-indigo-500'>Hi, my name is</div>
+            <div class='text-6xl text-slate-200'>Hezekiah Hendry.</div>
+            <div class='text-6xl text-slate-400'>I build things for the web and beyond.</div>
+          </div>
+
+          <div class='text-xl text-slate-400 max-w-2xl'>
+            I'm a backend developer specializing in building (and occasionally designing) amazing projects. Currently, I'm focused on building
+            something to fill this space in.
+          </div>
+        </div>
+      </div>
+
+      <div class='flex justify-center p-4 sm:p-8 lg:p-12'>
+        <div class='font-mono text-md text-slate-500'>
+          <div>Designed & Built by Hezekiah Hendry</div>
+        </div>
+      </div>
     </div>
   );
 }
