f746efd8560514d98968d45d767892c6beb2c750
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-05-26 01:59:14 -0400

    fix: padding and responsive text

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,17 @@ export function App() {
       <NavigationBar />
 
       <div class='flex justify-center'>
-        <div class='max-w-4xl space-y-8 px-8'>
+        <div class='max-w-4xl space-y-8 p-8'>
           <div class='space-y-4'>
-            <div class='text-base sm:text-xl font-mono text-indigo-500'>Hi, my name is</div>
-            <div class='text-4xl sm:text-6xl'>Hezekiah Hendry.</div>
-            <div class='text-4xl sm:text-6xl'>I build things for the web and beyond.</div>
+            <h3 class='text-base sm:text-xl font-mono text-indigo-500'>Hi, my name is</h3>
+            <h1 class='text-4xl sm:text-6xl'>Hezekiah Hendry.</h1>
+            <h1 class='text-4xl sm:text-6xl'>I build things for the web and beyond.</h1>
           </div>
 
-          <div class='text-base sm:text-xl'>
-            I'm a backend developer specializing in building (and occasionally designing) amazing projects. Currently, I'm focused on building
+          <p class='text-base sm:text-xl'>
+            I'm a back-end developer specializing in building (and occasionally designing) amazing projects. Currently, I'm focused on building
             something to fill this space in.
-          </div>
+          </p>
         </div>
       </div>
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <div class='flex justify-center'>
-      <div class='font-mono text-sm text-slate-500'>
+      <div class='font-mono text-sm p-8 text-slate-500'>
         <div>Designed & Built by Hezekiah Hendry</div>
       </div>
     </div>
