0eb6367ba636bb445d9809a9580017586a126126
Hezekiah Hendry <hezekiah.hendry@gmail.com>
2022-05-26 01:49:56 -0400

    refactor: move navbar and footer to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,27 @@
+import { Footer } from './components/Footer';
+import { NavigationBar } from './components/NavigationBar';
+
 export function App() {
   return (
-    <div class='text-white bg-black w-full h-full'>
-      <nav class='sticky top-0 pt-4'>
-        <div class='absolute right-0 mr-8 sm:mr-12 lg:mr-16'>
-          <div class='flex space-x-4'>
-            <a
-              href='https://discord.gg/fERY6AenEv'
-              target='_blank'
-              class='hover:bg-indigo-600 transition ease-in-out rounded-lg block w-auto px-3 py-2'
-            >
-              <img src='../assets/discord.png' height='32' width='32' alt='Discord' />
-            </a>
+    <div class='bg-black text-white space-y-24'>
+      <NavigationBar />
 
-            <a
-              href='https://github.com/RealShadowNova'
-              target='_blank'
-              class='hover:shadow-xl hover:bg-indigo-600 transition ease-in-out rounded-lg block w-auto px-3 py-2'
-            >
-              <img src='../assets/github.png' alt='GitHub' />
-            </a>
-          </div>
-        </div>
-      </nav>
-
-      <div class='container min-h-screen p-12 sm:p-24 lg:p-32'>
-        <div class='space-y-8'>
+      <div class='flex justify-center'>
+        <div class='max-w-4xl space-y-8 px-8'>
           <div class='space-y-4'>
-            <div class='text-xl font-mono text-indigo-500'>Hi, my name is</div>
-            <div class='text-6xl text-slate-200'>Hezekiah Hendry.</div>
-            <div class='text-6xl text-slate-400'>I build things for the web and beyond.</div>
+            <div class='text-base sm:text-xl font-mono text-indigo-500'>Hi, my name is</div>
+            <div class='text-4xl sm:text-6xl'>Hezekiah Hendry.</div>
+            <div class='text-4xl sm:text-6xl'>I build things for the web and beyond.</div>
           </div>
 
-          <div class='text-xl text-slate-400 max-w-2xl'>
+          <div class='text-base sm:text-xl'>
             I'm a backend developer specializing in building (and occasionally designing) amazing projects. Currently, I'm focused on building
             something to fill this space in.
           </div>
         </div>
       </div>
 
-      <div class='flex justify-center p-4 sm:p-8 lg:p-12'>
-        <div class='font-mono text-md text-slate-500'>
-          <div>Designed & Built by Hezekiah Hendry</div>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <div class='flex justify-center'>
+      <div class='font-mono text-sm text-slate-500'>
+        <div>Designed & Built by Hezekiah Hendry</div>
+      </div>
+    </div>
+  );
+}
